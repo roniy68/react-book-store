6650b5e6aca9d8aa5a95f74997e78b495141f28a
roniy68 <ahmed.11900449@lpu.in>
2023-03-04 00:08:08 +0530

    🔥 Check Changes

--- src/pages/CategoriesPage.js
+++ src/pages/CategoriesPage.js
@@ -4,7 +4,7 @@ function CategoriesPage() {
   return (
     <div>
       <div className="categories-wrapper row">
-        <button type="button" onClick={() => console.log('Check Status is under construction')}>Check status</button>
+        <button type="button">Check status</button>
       </div>
     </div>
   );
