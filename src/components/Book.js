c1c8ebc11657e63b9b46372c4c09e98467a0bed9
roniy68 <ahmed.11900449@lpu.in>
2023-03-03 22:10:13 +0530

    🔥 Add BookStore API

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { booksActions } from '../redux/books/booksSlice';
+import { booksActions, deleteBook } from '../redux/books/booksSlice';
 
 function Book(props) {
   const dispatch = useDispatch();
@@ -9,8 +9,10 @@ function Book(props) {
   const removeBookHandler = (e) => {
     // Dispatch remove book action
     const { id } = e.target.dataset;
-
     dispatch(booksActions.removeBook(id));
+
+    // Delete in the backend
+    dispatch(deleteBook(id));
   };
 
   return (
