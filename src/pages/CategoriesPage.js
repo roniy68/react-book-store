8cd5ee99d4429bc0c8444aa2690a54dfe56e389e
roniy68 <ahmed.11900449@lpu.in>
2023-03-04 00:58:20 +0530

    🔥 Changes Made to the categories page

--- src/pages/CategoriesPage.js
+++ src/pages/CategoriesPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
 function CategoriesPage() {
+  const dispatch = useDispatch();
   return (
     <div>
       <div className="categories-wrapper row">
-        <button type="button">Check status</button>
+        <button type="button" onClick={() => dispatch(checkStatus("Under Construction"))}>Check status</button>
       </div>
     </div>
   );
