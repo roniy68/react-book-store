5a128464faca0c3dbe59edfe0873094c2b2214d0
roniy68 <ahmed.11900449@lpu.in>
2023-03-04 01:39:52 +0530

    🔥 Merged from conflicts

--- src/pages/CategoriesPage.js
+++ src/pages/CategoriesPage.js
@@ -1,4 +1,3 @@
-
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Book from '../components/Book';
@@ -36,6 +35,8 @@ function CategoriesPage() {
             />
           ))}
         </div>
+      </div>
+    </div>
   );
 }
 
